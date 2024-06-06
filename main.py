9e9aff28f5b261e4537f5b2e42e3b3ef0a70af10
TahaThinks <tahaa4587@gmail.com>
2024-06-06 21:11:11 +0400

    Get the prices

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from bs4 import BeautifulSoup
 product_url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
 
 response = requests.get(url=product_url)
-print(response.text)
+# print(response.text)
 
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title.text)
+price_digit = soup.find(name="span", class_="a-price-whole").text
+price_decimal = soup.find(name="span", class_="a-price-fraction").text
+
+price = float(f"{price_digit}{price_decimal}")
+print(price)
