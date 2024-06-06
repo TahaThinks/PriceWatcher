26ae7ba10af93fa156b20767648696c74b6d7b2f
TahaThinks <tahaa4587@gmail.com>
2024-06-06 20:49:02 +0400

    Get the URL HTML

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+
+product_url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+
+response = requests.get(url=product_url)
+print(response.text)
+
